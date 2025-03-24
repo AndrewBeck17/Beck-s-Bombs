f3c7617de6d5af433e5568db30325c24817b86fa
vang_0931809 <tenjivang@gmail.com>
2025-03-24 12:11:17 -0700

    added FPS for drawParticle function

--- ParticleSystem.h
+++ ParticleSystem.h
@@ -9,33 +9,33 @@ class ParticleSystem {
         Cell* head;
 		int amount = 0;
     public:
-        ParticleSystem() : head(nullptr) {}
+        ParticleSystem() : head(nullptr) {} //constructor
         ~ParticleSystem() {
         Cell* curr = head;
-        while (curr != nullptr) {
+        while (curr != nullptr) { //destructor
             Cell* next = curr->next;
             delete curr;
             curr = next;
         }
     }
-		int numParticles() {
+		int numParticles() { //get number of particles in list
 			return amount;
 		}
 
-        void addParticle(const Particle& particle) {
+        void addParticle(const Particle& particle) { //add a new particle to the list
             Cell* newP = new Cell(particle);
             newP->next = head;
             head = newP;
 			amount++;
         }
 
-        void updateDrawParticle() {
+        void updateDrawParticle() { //update the screen and draw the particles again
             Cell* curr = head;
             Cell* prev = nullptr;
             while (curr != nullptr) {
                 auto [rows,cols] = get_terminal_size();
 				curr->particle.physics(*this);	
-                if (curr->particle.x < 0) {
+                if (curr->particle.x < 0) { //delete if particle leaves screen
                     Cell* temp = curr;
                     curr = curr->next;
                     if (prev != nullptr) {
@@ -46,7 +46,7 @@ class ParticleSystem {
                     }
                     delete temp;
                 }
-                else if (curr->particle.y < 0) {
+                else if (curr->particle.y < 0) { //delete if particle leaves screen
                     Cell* temp = curr;
                     curr = curr->next;
                     if (prev != nullptr) {
@@ -57,7 +57,7 @@ class ParticleSystem {
                     }
                     delete temp;
                 } 
-                else if (curr->particle.x >= cols) {
+                else if (curr->particle.x >= cols) { //delete if particle leaves screen
                     Cell* temp = curr;
                     curr = curr->next;
                     if (prev != nullptr) {
@@ -68,7 +68,7 @@ class ParticleSystem {
                     }
                     delete temp;
                 }
-                else if (curr->particle.y >= rows) {
+                else if (curr->particle.y >= rows) { //delete if partricle leave screen
                     Cell* temp = curr;
                     curr = curr->next;
                     if (prev != nullptr) {
@@ -80,7 +80,7 @@ class ParticleSystem {
                     delete temp;
                 }
 
-                else if (curr->particle.lifetime <= 0) {
+                else if (curr->particle.lifetime <= 0) { //delete if particle's lifetime ends
                     Cell* temp = curr;
                     if (prev != nullptr) {
                         prev->next = curr->next;
@@ -100,7 +100,7 @@ class ParticleSystem {
         }
 
 		//changed function to allow inputs for particle look, particle color, and particle background color -- Andrew
-        void drawParticles(char look = 'O',int r = 255, int g = 255, int b = 255, int bgr = 0, int bgg = 0, int bgb = 0) {
+        void drawParticles(char look = 'O',int r = 255, int g = 255, int b = 255, int bgr = 0, int bgg = 0, int bgb = 0, int FPS = 120) {
             Cell* curr = head;
             while (curr != nullptr) {
                 movecursor(curr->particle.y, curr->particle.x);
@@ -108,7 +108,7 @@ class ParticleSystem {
 				setbgcolor(bgr,bgg,bgb);
                 cout << look;
                 cout.flush();
-                usleep(1'000'000 / 120);
+                usleep(1'000'000 / FPS);
                 if (curr->next == nullptr) clearscreen();
                 curr = curr->next;
             }
