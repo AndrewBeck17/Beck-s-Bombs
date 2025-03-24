6e5cd64f317ef3ce786d226141751441aeca6e53
q <[B[B[B[Aq>
2025-03-24 15:36:16 -0700

    completed beck.cc

--- beck.cc
+++ beck.cc
@@ -12,55 +12,45 @@
 
 
 void becks_bomb() {
-	int frames = 100;
-	ParticleSystem bombcase, b, o, m;
+	int frames = 60;
+	int fps = 120;
+	ParticleSystem bomb;
+	ParticleGraphics graphics(255,0,0);
+	//gets screen ready for particle graphic
 	clearscreen();
 	show_cursor(false);
 
-
 	//bomb casing particles
-	bombcase.addParticle(Particle(50, 0, 0, -1, frames - 30, MovementType::STREAMER));
-	bombcase.addParticle(Particle(50, 1, 0, -1, frames - 30, MovementType::STREAMER));
-	bombcase.addParticle(Particle(50, 1, 0, -1, frames - 30, MovementType::STREAMER));
-	bombcase.addParticle(Particle(50, 2, 0, -1, frames - 30, MovementType::STREAMER));
-	bombcase.addParticle(Particle(50, 3, 0, -1, frames - 30, MovementType::STREAMER));
-	bombcase.addParticle(Particle(50, 4, 0, -1, frames - 30, MovementType::STREAMER));
-	bombcase.addParticle(Particle(52, 0, 0, -1, frames - 30, MovementType::STREAMER));
-	bombcase.addParticle(Particle(52, 1, 0, -1, frames - 30, MovementType::STREAMER));
-	bombcase.addParticle(Particle(52, 2, 0, -1, frames - 30, MovementType::STREAMER));
-	bombcase.addParticle(Particle(52, 3, 0, -1, frames - 30, MovementType::STREAMER));
-	bombcase.addParticle(Particle(52, 4, 0, -1, frames - 30, MovementType::STREAMER));
-	bombcase.addParticle(Particle(51, 0, 0, -1, frames - 30, MovementType::STREAMER));
-	bombcase.addParticle(Particle(51, 4, 0, -1, frames - 30, MovementType::STREAMER));
-	//bomb particles	
-	b.addParticle(Particle(51, 1, 0, -1, frames - 30, MovementType::FIREWORK));
-	b.addParticle(Particle(51, 1, 0, -1, frames - 30, MovementType::FIREWORK));
-	b.addParticle(Particle(51, 1, 0, -1, frames - 30, MovementType::FIREWORK));
-	b.addParticle(Particle(51, 1, 0, -1, frames - 30, MovementType::FIREWORK));
-	b.addParticle(Particle(51, 1, 0, -1, frames - 30, MovementType::FIREWORK));
-	o.addParticle(Particle(51, 2, 0, -1, frames - 30, MovementType::FIREWORK));
-	o.addParticle(Particle(51, 2, 0, -1, frames - 30, MovementType::FIREWORK));
-	o.addParticle(Particle(51, 2, 0, -1, frames - 30, MovementType::FIREWORK));
-	o.addParticle(Particle(51, 2, 0, -1, frames - 30, MovementType::FIREWORK));
-	o.addParticle(Particle(51, 2, 0, -1, frames - 30, MovementType::FIREWORK));
-	m.addParticle(Particle(51, 3, 0, -1, frames - 30, MovementType::FIREWORK));
-	m.addParticle(Particle(51, 3, 0, -1, frames - 30, MovementType::FIREWORK));
-	m.addParticle(Particle(51, 3, 0, -1, frames - 30, MovementType::FIREWORK));
-	m.addParticle(Particle(51, 3, 0, -1, frames - 30, MovementType::FIREWORK));
-	m.addParticle(Particle(51, 3, 0, -1, frames - 30, MovementType::FIREWORK));
+	//note: exist for 23 frames
+	bomb.addParticle(Particle(77, 0, 0, -0.25, frames-3, MovementType::BALLISTIC));
+	bomb.addParticle(Particle(78, 0, 0, -0.25, frames-3, MovementType::BALLISTIC));
+	bomb.addParticle(Particle(79, 0, 0, -0.25, frames-3, MovementType::BALLISTIC));
+	bomb.addParticle(Particle(77, 1, 0, -0.25, frames-2, MovementType::BALLISTIC));
+	bomb.addParticle(Particle(78, 1, 0, -0.25, frames-2, MovementType::BALLISTIC));
+	bomb.addParticle(Particle(79, 1, 0, -0.25, frames-2, MovementType::BALLISTIC));
+	bomb.addParticle(Particle(77, 2, 0, -0.25, frames-1, MovementType::BALLISTIC));
+	bomb.addParticle(Particle(78, 2, 0, -0.25, frames-1, MovementType::BALLISTIC));
+	bomb.addParticle(Particle(79, 2, 0, -0.25, frames-1, MovementType::BALLISTIC));
+	bomb.addParticle(Particle(77, 3, 0, -0.25, frames, MovementType::BALLISTIC));
+	bomb.addParticle(Particle(78, 3, 0, -0.25, frames, MovementType::BALLISTIC));
+	bomb.addParticle(Particle(79, 3, 0, -0.25, frames, MovementType::BALLISTIC));
 
+	//updates screen rapidly for appearance of movement
 	for (int j = 0; j < frames; j++) {
-		bombcase.updateDrawParticle();
-		bombcase.drawParticles('I',255,255,255,0,0,0);
-		b.updateDrawParticle();
-		b.drawParticles('B',255,255,255,0,0,0);
-		o.updateDrawParticle();
-		o.drawParticles('O',255,255,255,0,0,0);
-		m.updateDrawParticle();
-		m.drawParticles('M',255,255,255,0,0,0);
+		bomb.updateDrawParticle();
+		bomb.drawParticles('o',255,255,255,0,0,0,fps);
+		graphics.drawRectangle(0,90,157,2,' ');
+		if (j == 23) {
+			//adds firework particles when the bomb hits the ground to simulate explosion
+			bomb.addParticle(Particle(77, 88, 0, 0, 21, MovementType::FIREWORK));
+			bomb.addParticle(Particle(78, 88, 0, 0, 21, MovementType::FIREWORK));
+			bomb.addParticle(Particle(79, 88, 0, 0, 21, MovementType::FIREWORK));
+			bomb.addParticle(Particle(76, 87, 0, 0, 21, MovementType::FIREWORK));
+			bomb.addParticle(Particle(80, 86, 0, 0, 21, MovementType::FIREWORK));
+		}
 	}
 
-
+	//resets screen for future inputs
 	clearscreen();
 	show_cursor(true);
 
