9446e7c3174c304cb83e8534a80b0d48db5119e0
vang_0931809 <tenjivang@gmail.com>
2025-03-24 14:55:04 -0700

    Added details

--- tenji.cc
+++ tenji.cc
@@ -19,7 +19,8 @@ double random_dbl() {
 void tenjis_function() {
 	//default variables
 	srand(time(0));
-	int frames = 110;
+	int frames = 500;
+	int lifetime = 110;
 	int fps = 240;
 	ParticleSystem ps;
 	clearscreen();
@@ -29,34 +30,38 @@ void tenjis_function() {
 	int j = 0;
 
 	//create creeper
-	ps.addParticle(Particle(1, 30, c_dx, 0, frames, MovementType::STREAMER));
-	ps.addParticle(Particle(1, 31, c_dx, 0, frames, MovementType::STREAMER));
-	ps.addParticle(Particle(1, 32, c_dx, 0, frames, MovementType::STREAMER));
-	ps.addParticle(Particle(1, 35, c_dx, 0, frames, MovementType::STREAMER));
-	ps.addParticle(Particle(1, 36, c_dx, 0, frames, MovementType::STREAMER));
-	ps.addParticle(Particle(3, 30, c_dx, 0, frames, MovementType::STREAMER));
-	ps.addParticle(Particle(3, 32, c_dx, 0, frames, MovementType::STREAMER));
-	ps.addParticle(Particle(3, 33, c_dx, 0, frames, MovementType::STREAMER));
-	ps.addParticle(Particle(3, 34, c_dx, 0, frames, MovementType::STREAMER));
-	ps.addParticle(Particle(3, 35, c_dx, 0, frames, MovementType::STREAMER));
-	ps.addParticle(Particle(5, 30, c_dx, 0, frames, MovementType::STREAMER));
-	ps.addParticle(Particle(5, 31, c_dx, 0, frames, MovementType::STREAMER));
-	ps.addParticle(Particle(5, 32, c_dx, 0, frames, MovementType::STREAMER));
-	ps.addParticle(Particle(5, 35, c_dx, 0, frames, MovementType::STREAMER));
-	ps.addParticle(Particle(5, 36, c_dx, 0, frames, MovementType::STREAMER));
+	ps.addParticle(Particle(1, 30, c_dx, 0, lifetime, MovementType::STREAMER));
+	ps.addParticle(Particle(1, 31, c_dx, 0, lifetime, MovementType::STREAMER));
+	ps.addParticle(Particle(1, 32, c_dx, 0, lifetime, MovementType::STREAMER));
+	ps.addParticle(Particle(1, 35, c_dx, 0, lifetime, MovementType::STREAMER));
+	ps.addParticle(Particle(1, 36, c_dx, 0, lifetime, MovementType::STREAMER));
+	ps.addParticle(Particle(3, 30, c_dx, 0, lifetime, MovementType::STREAMER));
+	ps.addParticle(Particle(3, 32, c_dx, 0, lifetime, MovementType::STREAMER));
+	ps.addParticle(Particle(3, 33, c_dx, 0, lifetime, MovementType::STREAMER));
+	ps.addParticle(Particle(3, 34, c_dx, 0, lifetime, MovementType::STREAMER));
+	ps.addParticle(Particle(3, 35, c_dx, 0, lifetime, MovementType::STREAMER));
+	ps.addParticle(Particle(5, 30, c_dx, 0, lifetime, MovementType::STREAMER));
+	ps.addParticle(Particle(5, 31, c_dx, 0, lifetime, MovementType::STREAMER));
+	ps.addParticle(Particle(5, 32, c_dx, 0, lifetime, MovementType::STREAMER));
+	ps.addParticle(Particle(5, 35, c_dx, 0, lifetime, MovementType::STREAMER));
+	ps.addParticle(Particle(5, 36, c_dx, 0, lifetime, MovementType::STREAMER));
 
 	//creeper travels across screen
 	for (int i = 0; i < frames; i++) {
-		movecursor(29, j);
-		cout << "HISSS...";
 		j++;
+		if (j > 90) { //when creeper reaches a certain distance say "HISSS"
+			movecursor(29, j);
+			cout << "HISSS...";
+		}
 		ps.updateDrawParticle();
 		ps.drawParticles('O', 0, 255, 0, 0, 0, 0, fps);
 	}
+	clearscreen();
 	//creeper explosion particles
 	for (int i = 0; i < 20; i++) {
-		ps.addParticle(Particle(110, 34, random_dbl(), random_dbl(), frames, MovementType::STREAMER));
+		ps.addParticle(Particle(110, 34, random_dbl(), random_dbl(), frames, MovementType::BALLISTIC));
 	}
+	clearscreen();
 	//update screen and draw explosion particles
 	for (int i = 0; i < frames; i++) {
 		movecursor(34, 110);
@@ -64,6 +69,7 @@ void tenjis_function() {
 		ps.updateDrawParticle();
 		ps.drawParticles('*', 255, 255, 255, 0, 0, 0, 240);
 	}
+
 	clearscreen();
 	show_cursor(true);
 
